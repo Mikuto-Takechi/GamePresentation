1a9ec833b9cb0c3e5986df34b8fae1cd4fca6db3
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2023-05-31 11:18:43 +0900

    no message

--- Assets/Scripts/VannerController.cs
+++ Assets/Scripts/VannerController.cs
@@ -7,7 +7,7 @@ public class VannerController : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             SceneManager.LoadScene("ClearScene");
         }
