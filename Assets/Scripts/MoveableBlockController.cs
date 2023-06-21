10d4c9cf3a32a568258eee4349194cc9889951c4
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2023-06-21 23:36:00 +0900

    簡単なステージを作成。 斧のダメージ処理を微調整。

--- Assets/Scripts/MoveableBlockController.cs
+++ Assets/Scripts/MoveableBlockController.cs
@@ -6,6 +6,7 @@ public class MoveableBlockController : MonoBehaviour
 {
     [SerializeField] float _messageDistance = 2.0f;
     [SerializeField] int _hitDamage = 3;
+    [SerializeField] ContactFilter2D filter2d;
     public int _throwSpeed = 10;
     public bool _projectile = false;
     private bool _deleteMessage = false;
@@ -24,6 +25,7 @@ public class MoveableBlockController : MonoBehaviour
     private void Update()
     {
         float distance = Vector2.Distance((Vector2)_player.transform.position, (Vector2)transform.position);
+        InTheAir();
 
         if (transform.position.y < -30f)
         {
@@ -59,6 +61,21 @@ public class MoveableBlockController : MonoBehaviour
                 enemy.DamageUI(_hitDamage);
             }
         }
-        if (collision.gameObject.tag != "Player") _projectile = false;
+    }
+    
+    void InTheAir()
+    {
+        if(gameObject.name == "Axe")
+        {
+            var isTouched = GetComponent<Rigidbody2D>().IsTouching(filter2d);
+            if(isTouched)
+            {
+                if(_projectile) _projectile = false;
+            }
+            else
+            {
+                if (!_projectile) _projectile = true;
+            }
+        }
     }
 }
